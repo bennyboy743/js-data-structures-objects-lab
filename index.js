6ce44d833731e161fb09f3aec72819ffd8297d23
Benjamin <benjaminrowe0@gmail.com>
2021-12-28 16:52:57 +1100

    have fixed the problem

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const driver = {
+    name:  "Sam",
+    address: "11 Broadway"
+}
+
+const updateDriverWithKeyAndValue = (driver, key, value) => {
+    // return driver[key] = value
+    const newDrivers = Object.assign({}, driver);
+    newDrivers[key] = value;
+    return newDrivers
+}
+
+const destructivelyUpdateDriverWithKeyAndValue = (driver, key, value) => {
+    driver[key] = value
+    return driver
+}
+
+const deleteFromDriverByKey = (driver, key) => {
+    const newCopyOfDrivers = Object.assign({}, driver)
+    delete newCopyOfDrivers[key]
+    return newCopyOfDrivers
+}
+
+const  destructivelyDeleteFromDriverByKey = (driver, key) => {
+    delete driver[key]
+    return driver
+}
+
